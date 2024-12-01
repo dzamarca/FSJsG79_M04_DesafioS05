2a04f6e718c50c2b1aa558b0408821d7b5372879
Darwin Zamarca G <dzamarca@subsole.com>
2024-12-01 16:26:14 -0300

    add - carga inicial

---
+++ src/views/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="bg-dark text-white  text-center p-3 my-3">
+        <p className="m-0">&copy; 2024 -Pizzería Mamma Mia! - Todos los derechos reservados - @dzamarca</p>
+      </div>
+  )
+}
+
+export default Footer
